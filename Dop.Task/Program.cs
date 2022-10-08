823f78d3e638e81270fcadd3b7d8d5fd2ed7bc98
Anna <anya-rage93@mail.ru>
2022-10-08 18:23:00 +0500

    16

--- Dop.Task/Program.cs
+++ Dop.Task/Program.cs
@@ -4,56 +4,61 @@
 void Zadacha1()
 {
     Console.Write("Введите число: ");
-    int num=Convert.ToInt32(Console.ReadLine());
-    for(int count=0; count<num;count++)
+    int num = Convert.ToInt32(Console.ReadLine());
+    for (int count = 0; count < num; count++)
     {
-        Console.Write(1+" ");
+        Console.Write(1 + " ");
     }
 }
 
 // Задача 2. Даны два числа a, b, такие что a < b. Вывести на экран сколько раз число a поместиться в числе b.
 void Zadacha2()
 {
-    Random random=new Random();
-    int a= random.Next(1,11);
-    int b=random.Next(20,100);
-    Console.Write($"Число {a} поместится в числе {b} ---> {b/a} раз ");
+    Random random = new Random();
+    int a = random.Next(1, 11);
+    int b = random.Next(20, 100);
+    Console.Write($"Число {a} поместится в числе {b} ---> {b / a} раз ");
 }
 
 // Задача 3. На ввод подаётся рост трёх спортсменов. Расположить их от большего к меньшему.
 void Zadacha3()
 {
-    
-    Random random=new Random();
-    int [] state=new int[3];
-    state[0]=random.Next(150,230);
-    state[1]=random.Next(150,230);
-    state[2]=random.Next(150,230);
-    Console.WriteLine($"Рост первого:{state[0]}\tРост второго:{state[1]}\tРост третьего:{state[2]}");
-    for(int i=1;i<state.Length;i++)
+    Random random = new Random();
+    int[] state = new int[3];
+    state[0] = random.Next(150, 230);
+    state[1] = random.Next(150, 230);
+    state[2] = random.Next(150, 230);
+    Console.WriteLine($"Рост первого:{state[0]} \t Рост второго:{state[1]} \t Рост третьего:{state[2]}");
+    for (int i = 0; i < state.Length - 1; i++)
     {
-        if(state[i]>state[i-1])
+        int maxState = i;
+        for (int j = i + 1; j < state.Length; j++)
         {
-            int tmp=state[i-1];
-            state[i-1]=state[i];
-            state[i]=tmp;
+            if (state[j] > state[maxState]) maxState = j;
         }
+        int temporary = state[i];
+        state[i] = state[maxState];
+        state[maxState] = temporary;
     }
-    
-    Console.Write($"Рост самого высокого:{state[0]}\tРост среднего:{state[1]}\tРост самого низкого:{state[2]}");
-    
+    Console.Write($"Рост самого высокого:{state[0]} \t Рост среднего:{state[1]} \t Рост самого низкого:{state[2]}");
 }
 
 // Задача 4. Дано N секунд. Вывести время в формате часы:минуты:секунды.
 // N = 72334 -> 20:5:34 
 void Zadacha4()
 {
+    Console.WriteLine("Введите количество секнд: ");
+    int time=Convert.ToInt32(Console.ReadLine());
     
+    int hours=time/3600;
+    int minuts=(time%360)/60;
+    int seconds=(time%360)%60;
+    Console.WriteLine($"Время----->  {hours}:{minuts}:{seconds} ");
 }
 
 // Zadacha1();
 // Zadacha2();
- Zadacha3();
+// Zadacha3();
 // Zadacha4();
 
 void TaskFriendsDog()
---
+++ Dop.Task7/Program.cs
@@ -0,0 +1,89 @@
+﻿void FillArray(int[,] numbers, int MinValue = 0, int MaxValue = 100)
+{
+    MaxValue++;
+    Random random = new Random();
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            numbers[i, j] = random.Next(MinValue, MaxValue);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int columns = numbers.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(numbers[i, j] + "\t");
+        }
+        Console.WriteLine();
+
+    }
+}
+
+// Задача 1. Даны две матрицы, количество строк и столбцов которых может быть 3 или 4,
+// заполнены числами от -9 до 9. Выполните умножение матриц.
+void Zadacha1()
+{
+
+    Random random = new Random();
+    int rows = 4;//random.Next(3, 5);
+    int columns = 4; //random.Next(3, 5);
+    int[,] arrayOne = new int[rows, columns];
+    FillArray(arrayOne, 1, 9);
+    PrintArray(arrayOne);
+    Console.WriteLine();
+
+    rows = 3;//random.Next(3, 5);
+    columns = 4;//random.Next(3, 5);
+    int[,] arrayTwo = new int[rows, columns];
+    FillArray(arrayTwo, 1, 9);
+    PrintArray(arrayTwo);
+
+    if (arrayOne.GetLength(1) == arrayTwo.GetLength(0) || arrayOne.GetLength(0) == arrayTwo.GetLength(1))
+    {
+        rows = arrayOne.GetLength(0);
+        columns = arrayTwo.GetLength(1);
+        int[,] arrayProduct = new int[rows, columns];
+        Console.WriteLine();
+        for (int k = 0; k < arrayProduct.GetLength(0); k++)
+        {
+            for (int m = 0; m < arrayProduct.GetLength(1); m++)
+            {
+                for (int j = 0; j < arrayTwo.GetLength(0); j++)
+                {
+                    Console.WriteLine($"{arrayOne[k, j]} * {arrayTwo[j, m]} \t{arrayProduct[k, m] + arrayOne[k, j] * arrayTwo[j, m]}");
+                    arrayProduct[k, m] = arrayProduct[k, m] + arrayOne[k, j] * arrayTwo[j, m];
+                }
+            }
+        }
+        Console.WriteLine();
+        PrintArray(arrayProduct);
+    }
+    else Console.WriteLine("Данные матрицы невозможно перемножить");
+}
+
+// Задача 2. Двумерный массив размером 3х4 заполнен числами от 100 до 9999. 
+// Найдите в этом массиве и сохраните в одномерный массив все числа, сумма цифр которых больше их произведения. Выведите оба массива.
+void Zadacha2()
+{
+
+}
+
+// Задача 3. Двумерный массив размером 5х5 заполнен случайными нулями и единицами. 
+// Игрок может ходить только по полям, заполненным единицами. Проверьте, существует ли путь из точки [0, 0] в точку [4, 4] (эти поля требуется принудительно задать равными единице).
+void Zadacha3()
+{
+
+}
+
+Zadacha1();
+// Zadacha2();
+// Zadacha3();
