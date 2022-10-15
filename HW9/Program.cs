0f3cb7fbc62968c13130952709b217b2937982ca
Anna <anya-rage93@mail.ru>
2022-10-15 17:39:29 +0500

    22

--- HW9/Program.cs
+++ HW9/Program.cs
@@ -21,21 +21,21 @@ int SumNatural(int from, int to, int sum = 0)
 }
 
 int Accerman(int m, int n)
+{
+    if (m == 0)
     {
-        if (m == 0)
-        {
-            return n + 1;
-        }
-        else if (n == 0)
-        {
-            return Accerman(m - 1, 1);
-        }
-        else 
-        {
-            return Accerman(m - 1, Accerman(m, n - 1));
-        }
+        return n + 1;
     }
-    
+    else if (n == 0)
+    {
+        return Accerman(m - 1, 1);
+    }
+    else
+    {
+        return Accerman(m - 1, Accerman(m, n - 1));
+    }
+}
+
 // Задача 64: Задайте значения M и N. Напишите рекурсивный метод, который выведет все натуральные числа кратные 3-ём в промежутке от M до N.
 void Zadacha64()
 {
