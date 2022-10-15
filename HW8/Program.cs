693428767c1e6731154bb548b8b02a450a499c18
Anna <anya-rage93@mail.ru>
2022-10-15 13:02:35 +0500

    19

--- HW8/Program.cs
+++ HW8/Program.cs
@@ -28,6 +28,16 @@ void PrintArray(int[,] numbers)
     }
 }
 
+void PrintOneDimensionalArray(int[] numbers) // Печать массива
+{
+    Console.WriteLine("Массив сумм элементов в строке ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + "\t");
+    }
+    Console.WriteLine();
+}
+
 void SortElementsRows(int[,] numbers, int rows, int columns)
 {
     for (int i = 0; i < rows; i++)
@@ -46,6 +56,30 @@ void SortElementsRows(int[,] numbers, int rows, int columns)
         }
     }
 }
+
+int MinSumInRows(int[,] numbers, int rows, int columns)
+{
+    int[] sumInRows = new int[rows];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            sumInRows[i] = sumInRows[i] + numbers[i, j];
+        }
+    }
+    int min = sumInRows[0];
+    int indexMin = 0;
+    for (int i = 0; i < sumInRows.Length; i++)
+    {
+        if (sumInRows[i] < min)
+        {
+            min = sumInRows[i];
+            indexMin = i;
+        }
+    }
+    return indexMin;
+}
+
 // Задача 54: Задайте двумерный массив. Напишите программу, 
 // которая упорядочит по убыванию элементы каждой строки двумерного массива.
 // Например, задан массив:
@@ -65,7 +99,7 @@ void Zadacha54()
     FillArray(numbers);
     PrintArray(numbers);
     Console.WriteLine();
-    SortElementsRows(numbers,rows,columns);
+    SortElementsRows(numbers, rows, columns);
     PrintArray(numbers);
 }
 // Задача 56: Задайте прямоугольный двумерный массив. 
@@ -79,12 +113,14 @@ void Zadacha54()
 void Zadacha56()
 {
     var random = new Random();
-    int rows = random.Next(4, 8);
-    int columns = random.Next(4, 8);
+    int rows = random.Next(4, 6);
+    int columns = random.Next(6, 10);
     int[,] numbers = new int[rows, columns];
-    FillArray(numbers);
+    FillArray(numbers, 0, 10);
     PrintArray(numbers);
     Console.WriteLine();
+    int indexMin = MinSumInRows(numbers, rows, columns);
+    Console.WriteLine($"Cтрока с наименьшей суммой элементов: {indexMin + 1} строка (счет идет от 1)");
 }
 
 // Задача 58: Напишите программу, которая заполнит спирально массив 4 на 4.
@@ -95,9 +131,29 @@ void Zadacha56()
 // 10 09 08 07
 void Zadacha58()
 {
+    int rows = 4;
+    int columns = 4;
+    int[,] numbers = new int[rows, columns];
+    int count = 1;
+    PrintArray(numbers);
+    Console.WriteLine();
 
+    for (int j = 0, i = 0; j < columns; j++, count++) numbers[i, j] = count;
+
+    for (int j = columns - 1, i = 1; i < rows; i++, count++) numbers[i, j] = count;
+
+    for (int j = columns - 2, i = rows - 1; j >= 0; j--, count++) numbers[i, j] = count;
+
+    for (int j = 0, i = rows - 2; i > 0; i--, count++) numbers[i, j] = count;
+
+    for (int j = 1, i = rows - 3; j < columns - 1; j++, count++) numbers[i, j] = count;
+
+    for (int j = columns - 2, i = rows - 2; j > 0; j--, count++) numbers[i, j] = count;
+
+    PrintArray(numbers);
 }
 
-Zadacha54();
+// Zadacha54();
 // Zadacha56();
-// Zadacha58();
+ Zadacha58();
+
---
+++ Seminar9/Program.cs
@@ -0,0 +1,65 @@
+﻿int InputMessage(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Numbers(int number,int counter=0)
+    {
+        Console.WriteLine(counter);
+        if (counter >= number) return;
+        counter++;
+        Numbers(number,counter);
+    }
+
+// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+void Zadacha63()
+{
+    int num=InputMessage("Введите число: ");
+    Numbers(num,1);
+}
+
+// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8" 
+void Zadacha65()
+{
+    int m=InputMessage("Введите число: ");
+    int n=InputMessage("Введите число: ");
+    Numbers(n,m);
+}
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+void Zadacha67()
+{
+    int numbers=InputMessage("Введите число: ");
+    int Sum(int num,int sum=0)
+    {
+        if(num<=0) return sum;
+        sum=sum+num%10;
+        num=num/10;
+        return Sum(num,sum);
+    }
+    Console.WriteLine(Sum(numbers));
+}
+
+// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8 
+void Zadacha69()
+{
+
+}
+
+// Zadacha63();
+// Zadacha65();
+ Zadacha67();
+// Zadacha69();
